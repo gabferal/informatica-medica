77ae6bc46fc9be658a7911860e25a32cf9628b40
Gabriel Alvarez <ec.gabrielalvarez@gmail.com>
2025-07-27 21:51:17 -0300

    Update dos arquivos app.py e requirements.txt para o correto funcionamento da porta

--- app.py
+++ app.py
@@ -100,6 +100,6 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    # Para desarrollo local
-    port = int(os.environ.get('PORT', 5000))
+    # Para desarrollo local - usar puerto 8000
+    port = int(os.environ.get('PORT', 8000))
     app.run(debug=True, host='0.0.0.0', port=port)
